395c1fa67a5f00e4fd036f6478b245968ab546ef
Matheus Tavares <tavares.matheus.sp@gmail.com>
2025-12-09 17:33:39 -0300

    fix: changing from facade to contrac, adding string validation on attributes

--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -7,35 +7,41 @@ namespace App\Services\User;
 use App\Exceptions\BusinessLogicException;
 use App\Models\User;
 use App\Repositories\User\UserRepositoryInterface;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Contracts\Hashing\Hasher;
 
 class UserService implements UserServiceInterface
 {
     public function __construct(
         protected UserRepositoryInterface $userRepository,
-        protected Hash $hash
+        protected Hasher $hash
     ) {
         $this->userRepository = $userRepository;
         $this->hash = $hash;
     }
 
     /**
-     * @param array<string, mixed>
+     * @param  array<string, mixed>  $attributes
      * @return User
      */
     public function createUser(array $attributes): User
     {
         $userEmail = $attributes['email'];
+        if (! is_string($userEmail)) {
+            throw new BusinessLogicException('Invalid email.');
+        }
+
         $emailAlreadyExists = $this->userRepository->detailByEmail($userEmail);
         if ($emailAlreadyExists !== null) {
             throw new BusinessLogicException('Couldn\'t create the user.');
         }
 
-        $user = new User(
-            $attributes['name'],
-            $userEmail,
-            $this->hash->make($attributes['password']),
-        );
+        $password = $attributes['password'];
+        if (! is_string($password)) {
+            throw new BusinessLogicException('Invalid password.');
+        }
+
+        $attributes['password'] = $this->hash->make($password);
+        $user = new User($attributes);
 
         return $this->userRepository->create($user);
     }
