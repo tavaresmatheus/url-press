a2fb51df3f20ef6b8c407fbbe9ec3fb611223a15
Matheus Tavares <tavares.matheus.sp@gmail.com>
2025-12-09 16:53:49 -0300

    feat: adding the user repository contract and concrete

---
+++ app/Repositories/User/UserRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories\User;
+
+use App\Models\User;
+use App\Repositories\GenericRepository\GenericRepository;
+
+/**
+ * @extends GenericRepository<User, string>
+ */
+class UserRepository extends GenericRepository implements UserRepositoryInterface
+{
+    public function __construct(User $model)
+    {
+        return parent::__construct($model);
+    }
+
+    /**
+     * @param  string  $email
+     * @return User|null
+     */
+    public function detailByEmail(string $email): ?User
+    {
+        /** @var User|null $user */
+        $user = $this->model->newQuery()
+            ->where('email', $email)
+            ->first();
+
+        return $user;
+    }
+}
---
+++ app/Repositories/User/UserRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories\User;
+
+use App\Models\User;
+use App\Repositories\GenericRepository\GenericRepositoryInterface;
+
+/**
+ * @extends GenericRepositoryInterface<User, string>
+ */
+interface UserRepositoryInterface extends GenericRepositoryInterface
+{
+    /**
+     * @param  string  $email
+     * @return User|null
+     */
+    public function detailByEmail(string $email): ?User;
+}
