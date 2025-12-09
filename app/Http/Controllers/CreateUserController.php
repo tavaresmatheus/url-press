2f0c730154cd2e2ce79a271c373c47ade92c1329
Matheus Tavares <tavares.matheus.sp@gmail.com>
2025-12-09 16:57:50 -0300

    feat: adding the creation of user by the user service

--- app/Http/Controllers/CreateUserController.php
+++ app/Http/Controllers/CreateUserController.php
@@ -2,20 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Services\User\UserServiceInterface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class CreateUserController extends Controller
 {
+    public function __construct(protected UserServiceInterface $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    /**
+     * @param  Request  $request
+     * @return JsonResponse
+     */
     public function __invoke(Request $request): JsonResponse
     {
-        $user = User::create([
+        $request->validate(
+            [
+                'name' => 'required|max:255',
+                'email' => 'required|email:rfc',
+                'password' => [
+                    'required',
+                    'regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*\W).{11,30}$/',
+                ],
+            ],
+            [
+                'password.regex' => 'Your password must have a number, a lowercase letter, a uppercase letter, a special character and length between 11 to 30 characters.',
+            ]
+        );
+
+        $attributes = [
             'name' => $request->get('name'),
             'email' => $request->get('email'),
-            'password' => Hash::make($request->get('password')),
-        ]);
+            'password' => $request->get('password'),
+        ];
+
+        $user = $this->userService->createUser($attributes);
 
         return response()->json($user);
     }
