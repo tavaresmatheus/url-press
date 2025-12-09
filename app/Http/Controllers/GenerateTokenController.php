a4a93d2864e36e1a6a1d04e8c0bb6d069b9dff9b
Matheus Tavares <tavares.matheus.sp@gmail.com>
2025-12-09 18:36:47 -0300

    feat: enhancing the generate token controller

--- app/Http/Controllers/GenerateTokenController.php
+++ app/Http/Controllers/GenerateTokenController.php
@@ -2,32 +2,59 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
+use App\Exceptions\InvalidCredentialsException;
+use App\Models\User;
+use App\Services\User\UserServiceInterface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class GenerateTokenController extends Controller
 {
+    public function __construct(protected UserServiceInterface $userService)
+    {
+        $this->userService = $userService;
+    }
+
     /**
      * @param  Request  $request
      * @return JsonResponse
      *
-     * @throws Exception
+     * @throws InvalidCredentialsException
      */
     public function __invoke(Request $request): JsonResponse
     {
+        $request->validate(
+            [
+                'email' => 'required|email:rfc',
+                'password' => [
+                    'required',
+                    'regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*\W).{11,30}$/',
+                ],
+            ],
+            [
+                'password.regex' => 'Your password must have a number, a lowercase letter, a uppercase letter, a special character and length between 11 to 30 characters.',
+            ]
+        );
+
         $credentials = [
             'email' => $request->get('email'),
             'password' => $request->get('password'),
         ];
 
         if (Auth::attempt($credentials) === false) {
-            throw new Exception('Invalid credentials');
+            throw new InvalidCredentialsException('Invalid credentials.');
         }
 
         $user = Auth::user();
-        $token = $user->createToken('token')->plainTextToken;
+        if (! $user instanceof User) {
+            throw new InvalidCredentialsException('Authentication error.');
+        }
+
+        /** @var \Laravel\Sanctum\NewAccessToken $accessToken */
+        $accessToken = $user->createToken('token');
+
+        $token = $accessToken->plainTextToken;
 
         return response()->json([
             'message' => 'Login successful',
