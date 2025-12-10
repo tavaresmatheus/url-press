9a0a6dd4ed50e81ddad736f2496f7f96bcb7c99e
Matheus Tavares <tavares.matheus.sp@gmail.com>
2025-12-10 17:54:13 -0300

    feat: adding the User detail endpoint

---
+++ app/Http/Controllers/DetailUserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\User\UserServiceInterface;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class DetailUserController extends Controller
+{
+    public function __construct(protected UserServiceInterface $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function __invoke(Request $request, string $id): JsonResponse
+    {
+        $user = $this->userService->detailUser($id);
+        return response()->json($user);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\CreateUserController;
+use App\Http\Controllers\DetailUserController;
 use App\Http\Controllers\GenerateTokenController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', GenerateTokenController::class);
 
 Route::post('/users', CreateUserController::class);
-Route::post('/login', GenerateTokenController::class);
+Route::prefix('users')->middleware('auth:sanctum')->group(function () {
+    Route::get('/{id}', DetailUserController::class);
+});
