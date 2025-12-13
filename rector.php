0e5e4f100af1a876e87ebf4bce487aefdcb8f9a0
Matheus Tavares <tavares.matheus.sp@gmail.com>
2025-12-13 10:54:40 -0300

    chore: adding rector to improve code quality and PHP standards

---
+++ rector.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap',
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+    ->withPreparedSets(
+        typeDeclarations: true,
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarationDocblocks: true
+    );
